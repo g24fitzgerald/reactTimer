3db1b7f0e3706d180db7641c0db553beb45d2823
Gina Fitzgerald <g24fitzgerald@gmail.com>
2017-05-22 15:36:54 -0500

    added countdown tests and styles

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -36,5 +36,31 @@ describe('Countdown', () => {
         done(); //lets us perform asynchronous test
       }, 3001)
     });
+
+    it('should pause countdown on paused status', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused'); //triggers paused status
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done(); //lets us perform asynchronous test
+      }, 1001)
+    });
+
+    it('should reset count on stopped status', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');//resets new status to stopped and should restart countdown
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done(); //lets us perform asynchronous test
+      }, 1001)
+    });
   });
 });
