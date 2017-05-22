6a3d07b6463b30b4a9ba00cfdcaab25522104149
Gina Fitzgerald <g24fitzgerald@gmail.com>
2017-05-22 13:46:28 -0500

    added control and test

--- app/tests/components/Controls.test.jsx
+++ app/tests/components/Controls.test.jsx
@@ -10,5 +10,24 @@ describe('Controls', () => {
   it('should exst', () => {
     expect(Controls).toExist();
   });
-  
+
+  describe('render', () => {
+    it('should render pause when started', () => {
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $pauseButton = $el.find('button:contains(Pause)');
+
+      expect($pauseButton.length).toBe(1);
+    });
+  });
+
+  describe('render', () => {
+    it('should render start when paused', () => {
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="pause"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $startButton = $el.find('button:contains(Start)');
+
+      expect($startButton.length).toBe(1);
+    });
+  });
 });
